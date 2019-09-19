d55c9f9b4165e1a5e422cd386e072e522563405b
Belal <belal@belal-TUF-GAMING-FX504GD-FX80GD>
2019-09-19 13:34:38 +0200

    Ajout du SQL

--- Finesse.sql
+++ Finesse.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Client :  localhost:3306
--- Généré le :  Jeu 12 Septembre 2019 à 15:53
+-- Généré le :  Jeu 19 Septembre 2019 à 13:31
 -- Version du serveur :  5.7.27-0ubuntu0.18.04.1
 -- Version de PHP :  7.2.19-0ubuntu0.18.04.2
 
@@ -46,6 +46,13 @@ CREATE TABLE `message` (
   `id_u` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Contenu de la table `message`
+--
+
+INSERT INTO `message` (`id_m`, `message`, `reason`, `id_u`) VALUES
+(1, 'J\'aimerai cristiano ronaldo sur une peinture', 'Demande particulière', 1);
+
 -- --------------------------------------------------------
 
 --
@@ -69,9 +76,21 @@ CREATE TABLE `product` (
   `id_p` int(11) NOT NULL,
   `title` varchar(255) NOT NULL,
   `description` text NOT NULL,
-  `price` decimal(15,3) NOT NULL,
+  `price` decimal(15,2) NOT NULL,
   `image` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Contenu de la table `product`
+--
+
+INSERT INTO `product` (`id_p`, `title`, `description`, `price`, `image`) VALUES
+(27, 'Textures VERT', 'Experimentations, recherche de nouvelles textures x effets de matiere. Réalisé avec de la paraffine coulée sur papier. Peinture acrylique', '20.00', '/../uploads/1568642535.jpeg'),
+(28, 'Textures ROUGE', 'Experimentations, recherche de nouvelles textures x effets de matiere. Réalisé avec de la paraffine coulée sur papier. Peinture acrylique', '20.00', '/../uploads/1568642582.jpeg'),
+(29, 'Textures ORANGE', 'Experimentations, recherche de nouvelles textures x effets de matiere. Réalisé avec de la paraffine coulée sur papier. Peinture acrylique', '20.00', '/../uploads/1568642602.jpeg'),
+(30, 'Textures BLEU', 'Experimentations, recherche de nouvelles textures x effets de matiere. Réalisé avec de la paraffine coulée sur papier. Peinture acrylique', '20.00', '/../uploads/1568642640.jpeg'),
+(31, 'COLORS INTERACTIONS', 'Recherches sur les interactions de couleurs, en restant sur une répétition de motif simples et répétitifs.', '49.99', '/../uploads/1568643784.jpg'),
+(32, 'Installations avec lumières calques et peintures', 'Installations réalisée en s\'appuyant sur COLORS INTERACTIONS. Jeu avec les lumières, les transparences, les effets de matière etc. Recherche de l\'hypnotisant', '49.99', '/../uploads/1568644024.jpeg');
 
 -- --------------------------------------------------------
 
@@ -86,13 +105,31 @@ CREATE TABLE `users` (
   `firstname` varchar(255) DEFAULT NULL,
   `birthDate` date DEFAULT NULL,
   `password` varchar(255) NOT NULL,
-  `zipCode` int(11) DEFAULT NULL,
+  `zipCode` int(5) DEFAULT NULL,
   `city` varchar(150) DEFAULT NULL,
   `number` int(11) DEFAULT NULL,
-  `street` varchar(5) DEFAULT NULL,
+  `street` varchar(50) DEFAULT NULL,
   `mail` varchar(255) NOT NULL,
   `superuser` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Contenu de la table `users`
+--
+
+INSERT INTO `users` (`id_u`, `pseudo`, `name`, `firstname`, `birthDate`, `password`, `zipCode`, `city`, `number`, `street`, `mail`, `superuser`) VALUES
+(1, 'Administrateur', 'Siddique', 'Belal', NULL, '$2y$10$JV4Nn8iBOAuEeheaiQmfquVpEBI2Mh/hPklo/qpzVgrBq01tK/ZDK', 80000, 'Amiens', NULL, 'carnot', 'Belal@live.fr', 1),
+(2, 'Inscription', NULL, NULL, NULL, '$2y$10$mn.rrxxxYjhTggLQ9hGXPemu26INy76GTXzriWreflb4.CTrrojl.', NULL, NULL, NULL, NULL, 'Inscription@live.fr', 0),
+(3, 'Inscription', NULL, NULL, NULL, '$2y$10$ygXL4AeJ3hAoU6f/3iK6P.aWBuoz7whbMOUpkd0Up0rLq7yoZCvlS', NULL, NULL, NULL, NULL, 'Inscription@live.fr', 0),
+(4, 'Inscription', NULL, NULL, NULL, '$2y$10$YDiwg/q6nd6UldFRkMHlme7iGbN1jKamHVynDO5I1x5iNIft4VD.q', NULL, NULL, NULL, NULL, 'Inscription@live.fr', 0),
+(5, 'Inscription', NULL, NULL, NULL, '$2y$10$ofqEe9uq3h8CPCzexYTG1.pRN.6HZ.FLOg4AKDotISgg5q3v0akVa', NULL, NULL, NULL, NULL, 'Inscription@live.fr', 0),
+(6, 'Inscription', NULL, NULL, NULL, '$2y$10$x6al5kpU4vU2HshXOfKOMuH5UW1oPqfLCAnVlm3xDDsAqGT2/LA0q', NULL, NULL, NULL, NULL, 'Inscription@live.fr', 0),
+(7, 'Inscription', NULL, NULL, NULL, '$2y$10$NnSID9AyCuD8/37eCP7h8OE/SMBFDZrfGw6GKA1/njhMAcIYdDE7i', NULL, NULL, NULL, NULL, 'Inscription@live.fr', 0),
+(8, 'Inscription', NULL, NULL, NULL, '$2y$10$SnveeJ0j20MYivNctAJ6H.SRtgHfrtB7N4yjxYq4pRkDJt.eEt1S6', NULL, NULL, NULL, NULL, 'Inscription@live.fr', 0),
+(9, 'Inscription', NULL, NULL, NULL, '$2y$10$qOk1CEUiAwFOs9XMfomyYeKHPsqX9fTJJ8Qy9/TtbPkq.20Y7gOMu', NULL, NULL, NULL, NULL, 'Inscription@live.fr', 0),
+(10, 'Beloule', NULL, NULL, NULL, '$2y$10$4M2m91O7feXMuZe1g/fXBe9cVHWwInK4yaYtcSVdvv75KvHFWDZRC', NULL, NULL, NULL, NULL, '05050505@live.fr', 0),
+(11, 'Beloule', NULL, NULL, NULL, '$2y$10$MvJ2qpqjT9DfKtL6MCrVeegeadjkgwLUJcOhiuj5Lk8.xF0EcDRjG', NULL, NULL, NULL, NULL, '05050505@live.fr', 0),
+(12, 'Pierre', NULL, NULL, NULL, '$2y$10$YQx.Vk6e5PTaRjurVSmCX.6HiWGzew2qo3cwmfspDrTtQW8oJu7JO', NULL, NULL, NULL, NULL, 'okok@live.fr', 0);
 
 --
 -- Index pour les tables exportées
@@ -146,7 +183,7 @@ ALTER TABLE `comment`
 -- AUTO_INCREMENT pour la table `message`
 --
 ALTER TABLE `message`
-  MODIFY `id_m` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_m` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT pour la table `orders`
 --
@@ -156,12 +193,12 @@ ALTER TABLE `orders`
 -- AUTO_INCREMENT pour la table `product`
 --
 ALTER TABLE `product`
-  MODIFY `id_p` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_p` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
 --
 -- AUTO_INCREMENT pour la table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id_u` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_u` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 --
 -- Contraintes pour les tables exportées
 --
